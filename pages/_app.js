ab5f694883b037141aeab7d2329f486681e2a5b6
dani <bonmobp@gmail.com>
2024-07-13 01:14:54 +0300

    jdj

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,13 @@
 import "../styles/sass/main.scss";
 import { Analytics } from "@vercel/analytics/react";
-
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
     return (
         <>
-           
+            <Head>
+                <link rel="icon" href="/icon.png" />
+            </Head>
             <Component {...pageProps} />
             <Analytics />
         </>
