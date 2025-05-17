dcbd7a989a78fcbff1f4c21a8b8a4fe0b39f9e2a
Your Name <you@example.com>
2025-05-17 10:17:44 +0300

    Updated the homepage layout

--- components/Projects.js
+++ components/Projects.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faExternalLinkAlt } from "@fortawesome/free-solid-svg-icons";
-import { faGithub } from "@fortawesome/free-brands-svg-icons"; // Correct import
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
 import useIntersectionObserver from "./effect";
 
 const Projects = () => {
@@ -10,6 +10,7 @@ const Projects = () => {
     return (
         <section ref={ref} className={`projects component ${isVisible ? 'component-visible' : ''}`} id="projects">
             <h1 className="mainTitle">Projects</h1>
+
             <div className="project">
                 <img src="/img/portfolio.jpg" alt="Portfolio Space Theme" />
                 <div className="details">
@@ -22,6 +23,7 @@ const Projects = () => {
                     </a>
                 </div>
             </div>
+
             <div className="project">
                 <img src="/img/samsung.jpg" alt="Samsung E-Commerce" />
                 <div className="details">
@@ -30,6 +32,26 @@ const Projects = () => {
                     </a>
                     <h2>Samsung E-Commerce</h2>
                     <a href="https://samsungdemo.kesug.com/?i=1" target="_blank" rel="noopener noreferrer" className="demo-link">
+                        <FontAwesomeIcon icon={faExternalLinkAlt} />
+                    </a>
+                </div>
+            </div>
+
+            <div className="project">
+                <img src="/img/labwehfa.jpg" alt="LabwehFa Academy Website" />
+                <div className="details">
+                    <h2>LabwehFa Academy Website</h2>
+                    <a href="https://labwefa.site" target="_blank" rel="noopener noreferrer" className="demo-link">
+                        <FontAwesomeIcon icon={faExternalLinkAlt} />
+                    </a>
+                </div>
+            </div>
+
+            <div className="project">
+                <img src="/img/daneline.jpg" alt="Danéline L'immortalité Flowers Store" />
+                <div className="details">
+                    <h2>Danéline L'immortalité Flowers Store</h2>
+                    <a href="https://danelineflowers.art/" target="_blank" rel="noopener noreferrer" className="demo-link">
                         <FontAwesomeIcon icon={faExternalLinkAlt} />
                     </a>
                 </div>
